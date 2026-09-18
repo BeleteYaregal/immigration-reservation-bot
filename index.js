ab81806a5f9f5c0b93c7db6d5082c33834d5e5c8
unknown <16belete19@gmail.com>
2026-09-18 03:33:55 +0300

    feat: add server.js and index.js entrypoints for Render default start command

---
+++ index.js
@@ -0,0 +1,2 @@
+﻿// Entrypoint for cloud hosts looking for index.js
+require('./src/bot.js');
---
+++ server.js
@@ -0,0 +1,2 @@
+﻿// Entrypoint for Render and cloud hosts looking for server.js
+require('./src/bot.js');
